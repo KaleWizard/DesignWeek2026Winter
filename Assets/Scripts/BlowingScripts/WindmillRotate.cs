a29b6962dba0660bc6cdc97d2791e00ad2d6bacd
wardtyl <wardtyl@OA-S141-26>
2026-03-05 14:58:19 -0500

    Audio for blowing windmill works, needs changes to pitch and distance

--- Assets/Scripts/BlowingScripts/WindmillRotate.cs
+++ Assets/Scripts/BlowingScripts/WindmillRotate.cs
@@ -8,11 +8,22 @@ public class WindmillRotate : BlowBehaviour
 
     float angularSpeed = 0;
 
+    public AudioSource windmillAudio;
+    public AudioClip windmillClip;
     protected override void OnBlown(Vector3 blowDirection)
     {
         bool positiveRotate = Vector3.Dot(blowDirection, transform.right) > 0;
 
         angularSpeed += (positiveRotate ? 1 : -1) * blowDirection.sqrMagnitude * amplify;
+        
+        if (Mathf.Abs(angularSpeed) > 200f && !windmillAudio.isPlaying)
+        {
+            windmillAudio.clip = windmillClip;
+            windmillAudio.Play();
+        } else if (Mathf.Abs(angularSpeed) < 200f)
+        {
+            windmillAudio.Stop();
+        }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
